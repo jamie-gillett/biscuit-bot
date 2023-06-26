a69ab88642839b4de33c515e36c513d9af934aac
Jamie <jamie.gillett@gmail.com>
2023-06-26 09:31:04 +0100

    slow bot is slower

--- bot.py
+++ bot.py
@@ -49,8 +49,6 @@ class Upgrader(Thread):
                 except:
                     continue
 
-
-
 class Builder(Thread):
     def __init__(self) -> None:
         super().__init__(daemon=True)
@@ -61,28 +59,43 @@ class Builder(Thread):
         self.active = not self.active
 
     def run(self) -> None:
-        i = 1
         while True:
             if self.active:
-                print(i)
-                i += 1
-                for x in range(1718, 1725):
-                    for y in range(40, 1016, 7):
-                        if pyautogui.pixelMatchesColor(x, y, self.green):
+                for y in range(40, 1016, 8):
+                    for x in range(1718, 1728):
+                        if pyautogui.pixelMatchesColor(x, y, self.green, tolerance=15):
                             pyautogui.click(x,y)
 
+class Gold(Thread):
+    def __init__(self) -> None:
+        super().__init__(daemon=True)
+        self.target = 'images/gold.png'
+        self.active = False
 
+    def toggle_active(self) -> None:
+        self.active = not self.active
+
+    def run(self) -> None:
+        while True:
+            if self.active:
+                try:
+                    x,y = pyautogui.locateCenterOnScreen(self.target, confidence=0.8)
+                    pyautogui.click(x,y)
+                except:
+                    continue
 
 class BikiBot(Bot):
     def __init__(self) -> None:
         super().__init__()
         self.upgrader = Upgrader()
         self.builder = Builder()
+        self.gold = Gold()
 
     def start(self):
         self.keyboard.start()
         self.upgrader.start()
         self.builder.start()
+        self.gold.start()
         while self.running:
             if self.active:
                 pyautogui.click(x=290,y=450)
@@ -91,6 +104,8 @@ class BikiBot(Bot):
         super().toggle_active()
         self.upgrader.toggle_active()
         self.builder.toggle_active()
+        self.gold.toggle_active()
+
 
 
 if __name__ == "__main__":
